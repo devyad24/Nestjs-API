ebed8dad28ae730d47382c1c67c2a7d4d8eb8f7b
devyad24 <devyadav4211@gmail.com>
2023-12-17 23:11:22 +0530

    Add user dto.

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,28 +1,38 @@
-import { Controller, Get, Req, Post, Delete, Put, HttpCode } from '@nestjs/common';
+import { Controller, Get, Req, Post, Delete, Put, HttpCode, Param, Body, Query } from '@nestjs/common';
 import { Request } from 'express';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('users')
 export class UsersController{
     @Post()
     @HttpCode(204)
-    create(): string {
+    async create(@Body() createUserDto: CreateUserDto) {
         return 'This action adds a new user';
     }
 
-    @Delete()
-    @HttpCode(200)
-    delete(): string {
-        return 'This action deletes the user';
-    }
-
-    @Put()
-    @HttpCode(200)
-    update(): string {
-        return 'This action updates the user';
-    }
-
     @Get()
-    findAll(@Req() request: Request): string{
+    findAll(@Query() query: any) {
+        return `This action returns all users (limit: ${query.limit} items`;
+    }
+
+    @Get(':id')
+    @HttpCode(200)
+    findOne(@Param() params: any, @Req() request: Request): string{
+        console.log(`User ${params.id} is found`);
         return 'This action returns all users';
+    }
+
+    @Delete(':id')
+    @HttpCode(200)
+    deleteOne(@Param() params: any): string {
+        console.log(params.id);
+        return `This action deletes the user${params.id}`;
+    }
+
+    @Put(':id')
+    @HttpCode(200)
+    update(@Param() params: any): string {
+        console.log(params.id);
+        return `This action updates the user${params.id}`;
     }
 }
